cb415fc3d2b95b4f04b36782e2881c06c199b0f7
Tim Dysart <tdysart@tactcomplabs.com>
2026-09-15 12:18:06 -0400

    changes for partial workflow and tooling

--- scripts/check_docker.sh
+++ scripts/check_docker.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
+# Original list of needed binaries
+# NEEDED_BINARIES=(
+#   soda-opt
+#   soda-translate
+#   mlir-opt
+#   mlir-translate
+#   flatbuffer_translate
+#   tf-mlir-translate
+#   tf-opt
+#   torch-mlir-opt
+#   bambu
+#   openroad
+#   yosys
+# )
+
 # list of needed binaries
 NEEDED_BINARIES=(
   soda-opt
   soda-translate
   mlir-opt
   mlir-translate
-  flatbuffer_translate
-  tf-mlir-translate
-  tf-opt
-  torch-mlir-opt
   bambu
-  openroad
-  yosys
 )
 
 DOCKER_RUN="docker run -u $(id -u):$(id -g) -v $(pwd):$(pwd) -w $(pwd) --rm agostini01/soda"
